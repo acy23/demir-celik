88dd6480589f20984af8c86b6fdf555273384a58
Ahmet Can Yıldız <cyildiz@sabanciuniv.edu>
2022-07-14 19:53:02 +0300

    developmnts

--- service1/service1/settings.py
+++ service1/service1/settings.py
@@ -41,12 +41,17 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders',
+    'service1_api'
+    
+
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -130,3 +135,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
--- service1/service1_api/models.py
+++ service1/service1_api/models.py
@@ -1,3 +1,10 @@
+from unicodedata import category
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Basic_metals(models.Model):
+
+    category = models.CharField(max_length=100)
+    price = models.IntegerField()
+    parity = models.CharField(max_length=100)
+    date = models.DateTimeField(default=timezone.now,blank=False)
